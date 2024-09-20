05b931f1cdba9a3a789a410c9b1b2b9c2536dcca
mseptiawan <straidr222@gmail.com>
2024-09-20 11:13:54 +0700

    baseng

---
+++ index.js
@@ -0,0 +1,34 @@
+// inisialisasi modul http
+const http = require('http');
+
+// buat server
+const server = http.createServer((req,res)=>{
+    //res.writeHead(200,{'Content-Type':'text/html'});
+    //resizeTo.write('Selamat pagi');
+    
+    res.writeHead(200,{'Content-Type':'application/json'});
+
+    if(req.url === '/dosen'){
+        message = 'List data dosen';
+        data = ['Ahmad','Hafiz','Widi'];
+    } else if(req.url === '/mahasiswa'){
+        message = 'List data mahasiswa';
+        data = ['Kenny','Rizky','Akbar'];
+    } else {
+        message = 'Tidak ditemukan';
+        data = [];
+    }
+    res.write(JSON.stringify({
+        'massage' : message,
+        'data' : data,
+        'status' : 'success'
+    }));
+    res.end();
+} );
+
+const port = 3000;
+const host = 'localhost';
+
+server.listen(port,host,()=>{
+    console.log(`Server berjalan di http://${host}:${port}`);
+} );
