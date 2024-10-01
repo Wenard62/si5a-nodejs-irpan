6489e71d45efb0711d5717d26cbd986affad776c
Wenard62 <straidr222@gmail.com>
2024-10-01 11:21:02 +0700

    zilikiy
    freki

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express") //Impor modul express
 const app = express() //Inisialisasi express
 const expressLayout = require("express-ejs-layouts"); //Import modul express-ejs-layouts
-const port = 3004
+const port = 3007
 
 app.set("views", __dirname + "/views");
 app.set('view engine', 'ejs');
 
 app.use(expressLayout);
+app.use(express.static('public'));
 
 //route
 app.get("/" ,(req,res) => {
@@ -31,14 +32,14 @@ app.get("/" ,(req,res) => {
 app.get("/about" ,(req,res) => {
     //res.send("about");
     //res.sendFile(__dirname + "/aboutus.html");
-    res.render("about", {title: 'About Us', layout: 'main'});
+    res.render('aboutus', {title: 'About Us', layout: 'main'});
 });
 
 // route kontak
 app.get("/contact" ,(req,res) => {
     //res.send("contact us");
     //res.sendFile(__dirname + "/contact.html");
-    res.render('contact');
+    res.render('contact', {title: 'About Us', layout: 'main'});
 });
 
 app.get("/mahasiswa", (req,res)=>{
